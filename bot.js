1825a615eaa1d21255922bc3827d728a0e30c29b
AlopexTMNT <74680084+AlopexTMNT@users.noreply.github.com>
2021-01-11 13:33:04 +0200

    Add files via upload
    Updated bot

--- bot.js
+++ bot.js
@@ -15,13 +15,7 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-if (msg.content === '$about') {
-    msg.reply('I am just a random bot created by kostas456_YT#9842');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === '$whoisalopex') {
+  if (msg.content === 'who is alopex') {
     msg.reply('Alopex is a Mutant Red Fox and she apeared in TMNT 2012 TV Series. Sister of Tiger Claw.');
   }
 });
@@ -30,27 +24,27 @@ client.on('message', msg => {
 
 
 client.on('message', message => {
-  if (message.content === '$Alopex') {
+  if (message.content === 'Alopex') {
     const attachment = new MessageAttachment('./pics/alopex.png');
     message.channel.send(attachment);
   }
 });
 
 client.on('message', msg => {
-  if (msg.content === '$licence') {
+  if (msg.content === 'licence') {
     msg.reply('**You can read our licence here** https://github.com/AlopexTMNT/AlopexBot/blob/main/README.md *PS* its OPEN SOURCE');
   }
 });
 
 client.on('message', message => {
-  if (message.content === '$Trapped') {
+  if (message.content === 'Trapped') {
     const attachment = new MessageAttachment('./pics/trapped.jpg');
     message.channel.send(attachment);
   }
 });
 
 client.on('message', message => {
-  if (message.content === '$Mutated') {
+  if (message.content === 'Mutated') {
     const attachment = new MessageAttachment('./pics/mutated.png');
     message.channel.send(attachment);
   }
@@ -59,13 +53,13 @@ client.on('message', message => {
 
 client.on('message', message => {
   if (message.content === '$help') {
-    message.channel.send('Commands: **HELP** *for help* **serverip** *for my cool minecraft serever ip* **ping** *Ping* **mutated** *You Will see* **Alopex** *Secret* **Kids** *Same* **about** *About the bot* **user-info** *Get the info of your account (Other account features may come)* **invite** *A link to invite my bot* ')
+    message.channel.send('Commands: **HELP** *for help* **serverip** *for my cool minecraft serever ip* **ping** *Ping* **mutated** *You Will see* **Alopex** *Secret* **Kids** *Same* **about** *About the bot* **user-info** *Get the info of your account (Other account features may come)* **invite** *A link to invite my bot* *Some commands might need "$"*')
   }
 });
 
 client.on('message', message => {
   if (message.content === `$ping`) {
-    message.channel.send('Pong.');
+    message.channel.send('Pinging started\nPinging gentoo.org\n [...]\n UR PING SUCKS');
   } else if (message.content === `$beep`) {
     message.channel.send('Boop.');
   } else if (message.content === `$server`) {
@@ -172,11 +166,11 @@ client.on('message', message => {
   }
 });
 
-client.on('message', message => {
-  if (message.content === 'SPAM') {
-    message.channel.send('@everyone')
-  }
-});
+//client.on('message', message => {
+//  if (message.content === 'SPAM') {
+//    message.channel.send('SPAM')
+//  }
+//});
 
 client.on('message', message => {
   if (message.content === 'gay') {
@@ -198,7 +192,7 @@ client.on('message', message => {
 
 client.on('message', message => {
   if (message.content === 'FitMC') {
-    message.channel.send('Minecraft username: Fit Joined 2b2t: Idk sometime in 2013')
+    message.channel.send('ThE oLdEsT aNaRcHy SeRvEr In MiNeCrAfT 2b2t.org. popbob backdoor hausemaster mad hause prio ban brownman brownman now lag 2b2t bad bad bad and lock hausemaster paypal account Hausemaster is kil')
   }
 });
 
@@ -286,6 +280,230 @@ client.on('message', message => {
 client.on('message', message => {
   if (message.content === 'Joanna') {
     const attachment = new MessageAttachment('./pics/joanna.jpg');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+    if (message.content === 'About') {
+        message.channel.send('I am just a random bot created by kostas456_YT#9842. Btw this bot runs on Arch Linux. FLEX FLEX FLEX. Whould be a bigger flex if it run on Gentoo, will do that sometime. Arch Linux user FLEXING')
+    }
+})
+
+// If someone loves Dream send this message I love Dream
+client.on('message', message => {
+  if (message.content === 'I love Dream') {
+    message.channel.send('HA GAYYY')
+  }
+});
+
+// Best Rapper (Logic)
+client.on('message', message => {
+  if (message.content === 'Best Rapper') {
+    message.channel.send('Logic')
+  }
+});
+// Logic command
+// Use that GIF you sucker https://tenor.com/view/logic-gif-9064307
+client.on('message', message => {
+  if (message.content === 'Logic') {
+    message.channel.send('https://tenor.com/view/logic-gif-9064307')
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Best Linux Distro') {
+    message.channel.send('Gentoo | https://www.gentoo.org')
+  }
+});
+
+client.on('message', message => {
+    if (message.content === 'BLM') {
+        message.channel.send('Haha no. Black lives do not matter. | TRUMP 2024 and /pol/ and /b/ of https://4chan.org')
+    }
+})
+
+client.on('message', message => {
+  if (message.content === 'Joanna2') {
+    const attachment = new MessageAttachment('./pics/joanna2.jpg');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Loukouman') {
+    message.channel.send('Ειναι αεροπλανο, ειναι πουλι ; ΟΧΙ ειναι ο ΛΟΥΚΟΥΜΑΝ!')
+    const attachment = new MessageAttachment('./pics/loukouman.png');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Loukoumam') {
+    message.channel.send('Ειναι αεροπλανο, ειναι πουλι ; ΟΧΙ ειναι ο ΛΟΥΚΟΥΜΑΝ! | SOSTO COMMAND Loukouman')
+    const attachment = new MessageAttachment('./pics/loukouman.png');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === '!Loukoumam') {
+    message.channel.send('Ειναι αεροπλανο, ειναι πουλι ; ΟΧΙ ειναι ο ΛΟΥΚΟΥΜΑΝ! | SOSTO COMMAND Loukouman | TWRA ME !')
+    const attachment = new MessageAttachment('./pics/loukouman.png');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === '!Loukouman') {
+    message.channel.send('Ειναι αεροπλανο, ειναι πουλι ; ΟΧΙ ειναι ο ΛΟΥΚΟΥΜΑΝ!  | TWRA ME !')
+    const attachment = new MessageAttachment('./pics/loukouman.png');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Arch Linux') {
+    message.channel.send('BTW I use Arch')
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Joanna + Kostas') {
+    message.channel.send(':eyes: Yes? No? Who knows')
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Boobies') {
+    message.channel.send('BOOBIES!!!')
+    const attachment = new MessageAttachment('./pics/alopexboobies.png');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Joanna3') {
+    const attachment = new MessageAttachment('./pics/joanna3.jpg');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'yawn') {
+    const attachment = new MessageAttachment('./pics/yawn.jpg');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'music') {
+    const attachment = new MessageAttachment('./pics/music.png');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'WHAT') {
+    const attachment = new MessageAttachment('./pics/what.jpg');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Famous') {
+    const attachment = new MessageAttachment('./pics/famous.jpg');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'clown') {
+    const attachment = new MessageAttachment('./pics/clown.png');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'ok') {
+    const attachment = new MessageAttachment('./pics/okhoney.jpg');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Cry') {
+    const attachment = new MessageAttachment('./pics/cry.jpg');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Sad') {
+    const attachment = new MessageAttachment('./pics/sadpepe.png');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Gun') {
+    const attachment = new MessageAttachment('./pics/pepewithgun.jpg');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Eisai mia fatsa') {
+    const attachment = new MessageAttachment('./pics/face.jpg');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'why') {
+    const attachment = new MessageAttachment('./pics/why.jpg');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Oh no') {
+    const attachment = new MessageAttachment('./pics/worriedpepe.png');
+    message.channel.send(attachment);
+  }
+});
+
+// tellstorypepe.png
+
+client.on('message', message => {
+  if (message.content === 'Tell a story') {
+    const attachment = new MessageAttachment('./pics/tellstorypepe.png');
+    message.channel.send(attachment);
+  }
+});
+
+// nazipepekill.jpg
+
+client.on('message', message => {
+  if (message.content === 'I will kill you') {
+    const attachment = new MessageAttachment('./pics/nazipepekill.jpg');
+    message.channel.send(attachment);
+  }
+});
+
+// nazipepewcross
+
+client.on('message', message => {
+  if (message.content === 'Nazi pepe') {
+    const attachment = new MessageAttachment('./pics/nazipepewcross.jpg');
+    message.channel.send(attachment);
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Χαλάρωσε') {
+    message.channel.send('Χαλάρωσε μην το κάνεις')
+      const attachment = new MessageAttachment('./pics/xalarose.png');
     message.channel.send(attachment);
   }
 });
